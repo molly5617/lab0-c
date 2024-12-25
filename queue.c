73024158503390c320edf1d0a1e35531917248da
molly5617 <mollytsai5617@gmail.com>
2024-12-25 15:41:09 +0800

    Your commit message

--- queue.c
+++ queue.c
@@ -86,19 +86,29 @@ bool q_insert_tail(struct list_head *head, char *s)
     return true;
 }
 
-
+static inline element_t *remove_element(element_t *obj,
+                                        char *sp,
+                                        size_t bufsize)
+{
+    if (!obj)
+        return NULL;
+    if (sp && bufsize != 0) {
+        size_t len = strlen(obj->value);
+        if (len > bufsize - 1)
+            len = bufsize - 1;
+        memcpy(sp, obj->value, len);
+        sp[len] = '\0';
+    }
+    list_del(&obj->list);
+    return obj;
+}
 
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    if (!head || list_empty(head) || !sp)
+    if (!head || list_empty(head))
         return NULL;
-
-    element_t *p = list_entry(head->next, element_t, list);
-    strncpy(sp, p->value, bufsize - 1);
-    sp[bufsize - 1] = '\0';
-    list_del(&p->list);
-    return p;
+    return remove_element(list_first_entry(head, element_t, list), sp, bufsize);
 }
 
 
@@ -106,14 +116,9 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    if (!head || list_empty(head) || !sp)
+    if (!head || list_empty(head))
         return NULL;
-
-    element_t *p = list_entry(head->prev, element_t, list);
-    strncpy(sp, p->value, bufsize - 1);
-    sp[bufsize - 1] = '\0';
-    list_del(&p->list);
-    return p;
+    return remove_element(list_last_entry(head, element_t, list), sp, bufsize);
 }
 
 
